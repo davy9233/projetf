80e56187d2719b1e49666903c724e59dfd6a2016
davy <catalan.davy@free.fr>
2021-04-28 14:58:12 +0200

    les icones sources

--- dockerReact/visualocr/src/index.js
+++ dockerReact/visualocr/src/index.js
@@ -2,14 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import App1 from './logos/pythonLogo';
+import App2 from './logos/pytorchLogo';
+import App3 from './logos/djangoLogo';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('logo')
 );
+ 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App1 />
+  </React.StrictMode>,
+  document.getElementById('logo1')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App2 />
+  </React.StrictMode>,
+  document.getElementById('logo2')
+);
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App3 />
+  </React.StrictMode>,
+  document.getElementById('logo3')
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ dockerReact/visualocr/src/logos/pythonLogo.js
@@ -0,0 +1,21 @@
+import logo from '../svg/python.png';
+
+function App1() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://www.python.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App1;
+  
